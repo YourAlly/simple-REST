a806e264e6a6755c46f6b525f3b489556287e4e3
YourAlly <Royettemiranda@YAHOO.COM>
2020-08-19 17:38:55 +0800

    Structure done

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const PORT = 3000;
 app = express();
-
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 // Connect to the databse
 mongoose.connect('mongodb://localhost:27017/api',
     {
@@ -29,12 +29,6 @@ const userSchema = new mongoose.Schema({
         minlength: [1, 'Must at least contain a character!'],
         maxlength: [32, 'Must not exceed 32 characters!']
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: [1, 'Must at least contain a character!'],
-        maxlength: [32, 'Must not exceed 32 characters!']
-    },
     disabled: {
         type: Boolean,
         required: true
@@ -46,9 +40,98 @@ const User = new mongoose.model('User', userSchema);
 
 // Users route
 app.route('/users')
-    .get((req, res)=>{})
-    .post((req, res) => { })
-    .put((req, res) => { })
-    .delete((req, res) => { });
+
+    // Gets all or certain users where username === req.query.name
+    // and disabled === req.query.disabled
+    .get((req, res)=>{
+        const disabled = req.query.disabled ?
+                            req.query.disabled.toLowerCase() === 'true'? true : false 
+                        : false;
+
+        if (req.query.all === 'true'){
+            User.find((err, foundUsers) => {
+                if (!err) {
+                    res.json(foundUsers);
+                } else {
+                    res.json(err);
+                }
+            });
+
+        } else if (req.query.username){
+            User.find({
+                username: req.query.username, 
+                disabled: disabled
+                },
+                (err, foundUsers)=>{
+                    if (!err){
+                        res.json(foundUsers);
+                        
+                    } else {
+                        res.json(err);
+                    }
+            });
+
+        } else {
+            User.find({
+                disabled: disabled
+            },
+                (err, foundUsers) => {
+                    if (!err) {
+                        res.json(foundUsers);
+
+                    } else {
+                        res.json(err);
+                    }
+                });
+        } 
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            username: req.body.username,
+            disabled: false
+        });
+        newUser.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new User.");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+app.route('/user/:username')
+    
+    // Updates a User
+    .put((req, res) => {
+        if (req.body.disabled){
+        const disabled = req.body.disabled.toLowerCase() === 'true' ? true : false
+        User.update(
+            { username: req.params.username},
+            { $set: {disabled: disabled}},
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated the User");
+                }
+            }
+        );
+        }
+        else{
+            res.send("Please provide a value for disabled")
+        }
+    })
+
+    // Deletes a User
+    .delete((req, res) => {
+        User.deleteOne(
+            { username: req.params.username },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully deleted the User.");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    });
 
 app.listen(PORT, ()=> console.log('Server Started'));
